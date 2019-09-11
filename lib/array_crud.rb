6fe0a8e08ed57bcdbbf1f351263d359fa18aefc4
Jonah Brown <jswish02@gmail.com>
2019-09-11 05:20:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,11 +28,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
- ["wow", "I", "am", "really", "learning", "arrays!"]
-[0]
+last= ["wow", "I", "am", "really", "learning", "arrays!"]
+last[0]
 end
 
 def retrieve_last_element_from_array(array)
- ["wow", "I", "am", "really", "learning", "arrays!"]
-  [-1]
+last =["wow", "I", "am", "really", "learning", "arrays!"]
+ last[-1]
 end
